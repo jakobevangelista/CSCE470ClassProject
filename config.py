52882d711c4dd79686f387273ded092c38f9f11b
Jakob Evangelista <jakobevangelista@tamu.edu>
2022-04-23 13:26:24 -0500

    hid api keys and pulls 100 tweets

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = '5A1vDXpaO5Nb7IjvhlOKGJYHJ'
+consumer_secret = '3Bdy4gQSbKusUcrhoZEJ33sFgJP9uraZJ5gwPyTHJjoO4GkDXR'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFw4aAEAAAAAiF3oT5j8LgGLE%2Bv1S%2Bl0u8vdFwo%3DnuXCRPcOW18vdmRER9UFvC7cbMgCFsh26hA2JOyjOYcTIjm99H'
+access_token = '2855531916-lUxyjcAL14zQ1CtiBfhEFolju1uDb7MpPb9BvUR'
+access_token_secret = 'bLbIMxgbo366SEkqu4zxxZeQ6IXCK4Rev62Y3u27Cfy9b'
--- scraper.py
+++ scraper.py
@@ -3,17 +3,18 @@ import tweepy
 import pandas as pd
 import json
 import requests
+import config
 
-consumer_key = '5A1vDXpaO5Nb7IjvhlOKGJYHJ'
-consumer_secret = '3Bdy4gQSbKusUcrhoZEJ33sFgJP9uraZJ5gwPyTHJjoO4GkDXR'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFw4aAEAAAAAiF3oT5j8LgGLE%2Bv1S%2Bl0u8vdFwo%3DnuXCRPcOW18vdmRER9UFvC7cbMgCFsh26hA2JOyjOYcTIjm99H'
-access_token = '2855531916-lUxyjcAL14zQ1CtiBfhEFolju1uDb7MpPb9BvUR'
-access_token_secret = 'bLbIMxgbo366SEkqu4zxxZeQ6IXCK4Rev62Y3u27Cfy9b'
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+bearer_token = config.bearer_token
+access_token = config.access_token
+access_token_secret = config.access_token_secret
 
 client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret, return_type=requests.Response)
 
 query = "powerlifting -is:retweet"
-tweetText = client.search_recent_tweets(query, max_results=10)
+tweetText = client.search_recent_tweets(query, max_results=100)
 
 tweets_dict = tweetText.json()
 
