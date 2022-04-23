4b387a65cc82503613e315496eba692e7498b232
Jakob Evangelista <jakobevangelista@tamu.edu>
2022-04-23 17:01:51 -0500

    only english language queries

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ access_token_secret = config.access_token_secret
 client = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret, return_type=requests.Response)
 
 query = "powerlifting"
-finalQuery = query + str(" -is:retweet")
+finalQuery = query + str(" -is:retweet lang:en")
 
 tweets = client.search_recent_tweets(finalQuery, tweet_fields=['public_metrics'],sort_order="relevancy" ,max_results=100)
 
